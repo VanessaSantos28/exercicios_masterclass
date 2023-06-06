53b9219029976926efdd0d2fa2bc531832eddfbb
VanessaSantos28 <vanessamss28@gmail.com>
2023-06-06 08:21:19 -0300

    update

--- test/exercicios_masterclass_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:exercicios_masterclass/exercicios_masterclass.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
