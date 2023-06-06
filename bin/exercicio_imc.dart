9a916fd036aa1ffb9343fb34e8d884431d680fcd
VanessaSantos28 <vanessamss28@gmail.com>
2023-06-06 16:42:53 -0300

    update exercicio imc

--- bin/exercicio_imc.dart
+++ bin/exercicio_imc.dart
@@ -1,11 +1,24 @@
 import 'dart:io';
 
-void main(){
-print(calculoIMC(height : 1,68, weight : 58.0));
-}
+void main() {
+  print('Qual seu nome?');
+  String? name = stdin.readLineSync();
+  print('Qual sua altura?');
+  String? yourHeight = stdin.readLineSync();
+  print('Qual seu peso?');
+  String? yourWeight = stdin.readLineSync();
 
-double calculoIMC ({required double height, required double weight}){
-double imc =  weight / (height * height);
-return imc;
-}
+  double height = double.parse(yourHeight!);
+  double weight = double.parse(yourWeight!);
 
+  double imc = weight / (height * height);
+  print(imc);
+
+  if (imc > 25) {
+    print("vc esta gordo");
+  } else if (imc < 18) {
+    print("vc esta magro");
+  } else {
+    print("seu peso é ideal");
+  }
+}
