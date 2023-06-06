b6174c8c772690d38375533a1d2016927064d4c9
VanessaSantos28 <vanessamss28@gmail.com>
2023-06-06 08:14:02 -0300

    first commit

---
+++ bin/main.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+
+void main(){
+print(calculoIMC(height : 1,68, weight : 58.0));
+}
+
+double calculoIMC ({required double height, required double weight}){
+double imc =  weight / (height * height);
+return imc;
+}
---
+++ test/exercicios_masterclass_test.dart
@@ -0,0 +1,8 @@
+import 'package:exercicios_masterclass/exercicios_masterclass.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
